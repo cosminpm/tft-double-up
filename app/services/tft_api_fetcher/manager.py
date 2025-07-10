a2ab0591ad17d36947294f9406df0a5ef1b4d3fa
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2025-07-10 23:47:58 +0200

    feat (WIP): Add compositions creation

--- app/services/tft_api_fetcher/manager.py
+++ app/services/tft_api_fetcher/manager.py
@@ -1,6 +1,13 @@
+from typing import TYPE_CHECKING
+
+from bs4 import BeautifulSoup
 from httpx import AsyncClient
 
 from app.config import Settings
+from app.services.tft_api_fetcher.models.composition import Composition
+
+if TYPE_CHECKING:
+    from fastapi import Response
 
 settings = Settings()
 
@@ -13,5 +20,37 @@ async def fetch_tft_top_compositions(request_client: AsyncClient) -> list:
         request_client (AsyncClient): The AsyncClient used for the async requests.
 
     """
-    await request_client.get(f"{settings.tft_url}/tierlist/team-comps/")
+    response: Response = await request_client.get(f"{settings.tft_url}/tierlist/team-comps/")
+
+    html_content = response.content.decode("utf-8", errors="ignore")
+    soup: BeautifulSoup = BeautifulSoup(html_content, "html.parser")
+    parse_soup(soup)
+    return []
+
+
+def parse_soup(soup: BeautifulSoup) -> list[Composition]:
+    """
+    Parse beautful soup and transform it into a list of Compositions
+    Args:
+        soup:
+
+    Returns:
+
+    """
+    for team_div in soup.select(".team-portrait"):
+        composition = Composition.from_tag(team_div)
+
+        champions = []
+        for champ_div in team_div.select(".characters-item"):
+            champ_name = champ_div.select_one(".team-character-name")
+            if not champ_name:
+                continue
+
+            name_text_champ = champ_name.text.strip()
+
+            # Items (if any)
+            items_div = champ_div.select(".character-items img")
+            items = [img["alt"].strip() for img in items_div] if items_div else []
+
+            champions.append({"name": name_text_champ, "items": items})
     return []
--- app/services/tft_api_fetcher/models/composition.py
+++ app/services/tft_api_fetcher/models/composition.py
@@ -1,3 +1,4 @@
+from bs4 import Tag
 from pydantic import BaseModel, Field
 
 
@@ -7,12 +8,33 @@ class Item(BaseModel):
 
 class Champion(BaseModel):
     name: str
-    items: list[Item] = Field(default_factory=list)
+    items: set[Item] = Field(default_factory=set)
+
+    # Make comparison only between name
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        if not isinstance(other, Champion):
+            return False
+        return self.name == other.name
 
 
 class Composition(BaseModel):
-    id: int
     name: str
-    champions: list[Champion] = Field(default_factory=list)
+    champions: set[Champion] = Field(default_factory=set)
     tier: str
     play_style: str
+
+    @classmethod
+    def from_tag(cls, tag: Tag):
+        tier_soup = tag.select_one(".team-rank.tone")
+        tier = tier_soup.text.strip() if tag else None
+
+        name_soup = tag.select_one(".team-name-elipsis")
+        name = name_soup.contents[0].strip() if name_soup else None
+
+        play_style_soup = tag.select_one(".team-playstyle")
+        play_style = play_style_soup.text.strip() if play_style_soup else None
+
+        return cls(name=name, play_style=play_style, tier=tier)
