b6df5ce0e1aa8401e4e74a87809984d1892de3cd
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2025-08-10 17:14:26 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    tft_url: str = "http://127.0.0.2:9999/fake-tft"
+    tft_url: str = "https://tftactics.gg/"
